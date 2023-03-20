b83d58ba662fb38c63e003da354e628305a0349c
marinacjensenn <84468498+marinacjensenn@users.noreply.github.com>
2023-03-20 09:59:14 -0300

    Add files via upload

---
+++ Trab1_DSI/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div id="login">
+        <h2>Bem-vindo(a)!</h2>
+        <h3>Faça login ou <a href="registro.html">registre-se.</a></h3>
+        <form action="cliente.php" method="post">
+            <label for="user">Usuário:</label>
+            <input type="text" name="user" id="user"> <br>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha">
+        </form>
+    </div>
+</body>
+</html>
